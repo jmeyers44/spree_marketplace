fd934af177438f97ba3eed38efd548e8181a695f
Jonathan Meyers <jonathanmeyers@Jonathans-MacBook-Pro.local>
2018-09-18 17:54:35 -0400

    add stripe access token and refresh token

--- db/migrate/20180918184040_add_stripe_to_merchants.rb
+++ db/migrate/20180918184040_add_stripe_to_merchants.rb
@@ -2,5 +2,7 @@ class AddStripeToMerchants < ActiveRecord::Migration[5.1]
   def change
     add_column :spree_merchants, :stripe_publishable_key, :string
     add_column :spree_merchants, :stripe_user_id, :string
+    add_column :spree_merchants, :access_token, :string
+    add_column :spree_merchants, :refresh_token, :string
   end
 end
