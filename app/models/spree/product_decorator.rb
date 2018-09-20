62361b109344287e109a028d613f1acedd63f070
Jonathan Meyers <jonathanmeyers@Jonathans-MacBook-Pro.local>
2018-09-20 15:33:12 -0400

    add active to product self.whitelisted_ransackable_attributes

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -2,7 +2,7 @@ Spree::Product.class_eval do
 
   belongs_to :merchant, touch: true
   
-  self.whitelisted_ransackable_attributes = %w[description name slug discontinue_on merchant_id]
+  self.whitelisted_ransackable_attributes = %w[description name slug discontinue_on merchant_id active]
 
   # Returns true if the product has a drop shipping merchant.
   def merchant?
